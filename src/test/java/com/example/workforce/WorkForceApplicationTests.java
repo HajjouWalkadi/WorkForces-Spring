ac6ae8cf82fad2aadc867dd9c60d82f7c1116c65
hajjou walkadi <walkadihajjou153@gmail.com>
2023-10-30 16:23:59 +0100

    First Commit

---
+++ src/test/java/com/example/workforce/WorkForceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkForceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
